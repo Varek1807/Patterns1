eb45c038ed8be13cd344ca2e95097fabb347cd7c
Varek1807 <105634176+Varek1807@users.noreply.github.com>
2022-10-07 16:45:33 +0500

    Update DeliveryTest.java

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -31,13 +31,8 @@ class DeliveryTest {
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
+
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-        // String planningDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[placeholder=\"Дата встречи\"]").setValue(firstMeetingDate);
         $("[placeholder=\"Город\"]").setValue(validUser.getCity());
         $("[name='phone']").setValue(validUser.getPhone());
@@ -47,12 +42,7 @@ class DeliveryTest {
         $x("//div[text()= 'Успешно!']").should(Condition.visible, Duration.ofSeconds(15));
         $x("//div[contains(text(), 'Встреча успешно запланирована')]").shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(15));
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-        // String planningDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[placeholder=\"Дата встречи\"]").setValue(secondMeetingDate);
-        //$("[placeholder=\"Город\"]").setValue(validUser.getCity());
-        //$("[name='phone']").setValue(validUser.getPhone());
-        //$("[name='name']").setValue(validUser.getName());
-        //$("[data-test-id='agreement']").click();
         $$("button").find(Condition.exactText("Запланировать")).click();
         $("[data-test-id='replan-notification']").should(Condition.visible);
         $$("button").find(Condition.exactText("Перепланировать")).click();
